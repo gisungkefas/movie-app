3409263e3cfdcd88ea34236bda764c4dfeed4703
Kefas Gisung <gisungkefas@gmail.com>
2022-12-29 07:30:21 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import MovieBox from "./MovieBox";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const API_URL="http://api.themoviedb.org/3/movie/popular?api_key=bcc4ff10c4ff10c2939665232d75d8bf0ec093";
 
 function App() {
+
+    const [movies, setMovies]=useState([]);
+    const example = ["One"]
+
+    useEffect(() => {
+        fetch(API_URL)
+            .then((res)=>res.json())
+            .then(data=>{
+                console.log(data);
+                setMovies(data.results);
+            })
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="grid">
+          {example.map( (movieReq) =>
+              <MovieBox key={movieReq.id} {...movieReq}/>)}
+      </div>
     </div>
   );
 }
